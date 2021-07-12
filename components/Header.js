eadf30539695a4876755dce92d6058855b06255e
Aanika Rahman <aanikarahman@gmail.com>
2021-07-12 06:34:16 -0400

    refactor(header): update links

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,30 @@
+import Link from 'next/link'
+
 const Header = () => (
   <header>
     <nav>
-      <ul className="menu">
-        <li className="logo"><a href="#">ZyphrZyphr</a></li>
-        <li className="item"><a href="#services">Services</a></li>
-        <li className="item"><a href="#about-us">About Us</a></li>
-        <li className="item"><a href="#team">Team</a></li>
-        <li className="item"><a href="#portfolio">Portfolio</a></li>
-        <li className="item button"><a href="#contact-us">Contact Us</a></li>
-        <li className="toggle"><a><i className="fas fa-bars"></i></a></li>
+      <ul className='menu'>
+        <li className='logo'>
+          <Link href='#'><a>Zyphr</a></Link>
+        </li>
+        <li className='item'>
+          <Link href='#services'><a>Services</a></Link>
+        </li>
+        <li className='item'>
+          <Link href='#about-us'><a>About Us</a></Link>
+        </li>
+        <li className='item'>
+          <Link href='#team'><a>Team</a></Link>
+        </li>
+        <li className='item'>
+          <Link href='#portfolio'><a>Portfolio</a></Link>
+        </li>
+        <li className='item button'>
+          <Link href='#contact-us'><a>Contact Us</a></Link>
+        </li>
+        <li className='toggle'>
+          <a><i className='fas fa-bars'></i></a>
+        </li>
       </ul>
     </nav>
   </header>
