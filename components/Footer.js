e9630fc88829c97d54c28a6cc17fab907ed689fc
Aanika Rahman <aanikarahman@gmail.com>
2021-12-05 06:56:35 -0500

    chore(components): archive old components

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,6 @@
 const Footer = () => (
-  <footer className="footer-section">
-    <div className="row">
-      <div className="col-12 footer-copyright">
-        <p>© 2020 Zyphr Solutions Inc. All Rights Reserved.</p>
-        <p>Coded with 💛 by Matthew Chan & Adam Kulidjian</p>
-      </div>
-    </div>
+  <footer>
+    Placeholder footer.
   </footer>
 )
 
