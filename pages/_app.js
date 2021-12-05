d2da0240d4d8d70f060b5fdffd2e12085672a328
Aanika Rahman <aanikarahman@gmail.com>
2021-12-05 06:07:39 -0500

    chore(styles): archive old styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import '../styles/style.css'
-import '../styles/animate.min.css'
-import '../styles/bootstrap-grid.css'
-import '../styles/calendly-widget.css'
-import '../styles/google-fonts.css'
+// import '../styles/old-styles/normalize.css'
+// import '../styles/old-styles/style.css'
+// import '../styles/old-styles/animate.min.css'
+// import '../styles/old-styles/bootstrap-grid.css'
+// import '../styles/old-styles/calendly-widget.css'
+// import '../styles/old-styles/google-fonts.css'
 
 const App = ({ Component, pageProps }) => (
   <Component {...pageProps} />
