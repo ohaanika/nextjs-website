d8d8d8de936efac1ac2cdc36f68bd7b4c78d9333
Aanika Rahman <aanikarahman@gmail.com>
2021-12-06 04:19:56 -0500

    feat(components, styles): define header/footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,12 @@
+import PlaceholderIcon from "./placeholders/placeholderIcon"
+
 const Footer = () => (
   <footer>
-    Placeholder footer.
+    <p>© 2020 Zyphr Solutions Inc. All Rights Reserved.</p>
+    <div className="icons">
+      <PlaceholderIcon/><PlaceholderIcon/><PlaceholderIcon/><PlaceholderIcon/>
+    </div>
+    
   </footer>
 )
 
--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,53 @@
+import Link from 'next/link'
+import PlaceholderLogo from "./placeholders/placeholderLogo"
+
+const navItems = [
+  {
+    path: '#services',
+    label: 'Services',
+  },
+  {
+    path: '#about',
+    label: 'About',
+  },
+  {
+    path: '#team',
+    label: 'Team',
+  },
+  {
+    path: '#portfolio',
+    label: 'Portfolio',
+  },
+  {
+    path: '#testimonials',
+    label: 'Testimonials',
+  },
+  {
+    path: '#contact',
+    label: 'Contact',
+    cta: true,
+  },
+]
+
+function createNavLink(navItem, i) {
+  var navLink = <Link href={navItem.path}><a>{navItem.label}</a></Link>
+  if ('cta' in navItem && navItem.cta) {
+    navLink = <button>{navLink}</button>
+  }
+  var navLink = <li key={i}>{navLink}</li>
+  return navLink
+}
+
 const Header = () => (
   <header>
-    Placeholder header.
+    <div>
+      <PlaceholderLogo/>
+    </div>
+    <nav>
+      <ul>
+        {navItems.map((navItem, i) => createNavLink(navItem, i))}
+      </ul>
+    </nav>
   </header>
 )
 
