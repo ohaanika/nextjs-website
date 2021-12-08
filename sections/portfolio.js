4f98aacbdaf7935001c147558c3d9204bd5b83e6
Aanika Rahman <aanikarahman@gmail.com>
2021-12-08 08:13:23 -0500

    feat(sections): add portfolio info (except img)

--- sections/portfolio.js
+++ sections/portfolio.js
@@ -1,5 +1,32 @@
 import Link from 'next/link'
 
+const PortfolioInfo = [
+  {
+    title: "Minty Budget App",
+    desc: "Gain insights into your budgets and finances.",
+    link: "https://minty-budget-tool.herokuapp.com/",
+    img: "",
+  },
+  {
+    title: "NYC Complaints",
+    desc: "Dashboard of borough complaints, with an SQL and chart editor inside.",
+    link: "https://dash-demo.plotly.host/dash-nyc-complaints/",
+    img: "",
+  },
+  {
+    title: "Admin Panel",
+    desc: "Manage the overall usage metrics of your Dash Enterprise server.",
+    link: "https://dash-demo.plotly.host/dash-admin-panel/",
+    img: "",
+  },
+  {
+    title: "COVID-19 Interventions",
+    desc: "Simulate the impact of custom non-pharmaceutical interventions on the dynamics of COVID-19.",
+    link: "https://intervention-model.herokuapp.com/",
+    img: "",
+  },
+]
+
 const Portfolio = () => (
   <section id="portfolio">
     <div>
