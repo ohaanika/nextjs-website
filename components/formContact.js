ea43e4f277db434a0c0f6ac7c5c8232fbdc3e4ab
Aanika Rahman <aanikarahman@gmail.com>
2021-12-09 12:16:18 -0500

    feat(components, sections): add contact form

---
+++ components/formContact.js
@@ -0,0 +1,82 @@
+const FormFieldInfo = [
+  {
+    label: "Name",
+    id: "name",
+    wrapper: "input",
+    type: "text",
+    required: true,
+    placeholder: "Your full name...",
+  },
+  {
+    label: "Affiliation",
+    id: "affiliation",
+    wrapper: "input",
+    type: "text",
+    required: true,
+    placeholder: "Your company name...",
+  },
+  {
+    label: "Email",
+    id: "email",
+    wrapper: "input",
+    type: "email",
+    required: true,
+    placeholder: "Your email address...",
+  },
+  {
+    label: "Inquiry",
+    id: "inquiry",
+    wrapper: "textarea",
+    type: "text",
+    required: true,
+    placeholder: "What's your story?",
+  },
+]
+
+function displayFormField(FormField, i=null) {
+  let label_required = <span style={{color: "var(--color-primary)"}}>
+    {(FormField.required === true) ? '＊' : ''}
+  </span>
+
+  return (
+    [
+      <div className="row pb-2" key={i}>
+        <label htmlFor={FormField.id}>{FormField.label} {label_required}</label>
+      </div>, 
+      <div className="row pb-4" key={i}>
+        <FormField.wrapper id={FormField.id} name={FormField.id} type={FormField.type} required></FormField.wrapper>
+      </div>
+    ]
+  )
+}
+
+const FormContact = () => {
+  return (
+    <div className="p-5" style={styles.FormContact}>
+      <form name="contact-form" method="POST" action="contact/?success=true">
+          {FormFieldInfo.map((FormField, i) => displayFormField(FormField, i))}
+          <div className="row pb-2">
+            <h6><label for="method">How did you hear about us?</label></h6>
+          </div>
+          <div className="row pb-4">
+            <select name="method" id="method">
+              <option disabled selected value> -- Select an Option -- </option>
+              <option value="recommendation">Recommendation</option>
+              <option value="google">Google Search</option>
+              <option value="social-media">Social Media</option>
+            </select>
+            </div>
+          <button type="submit" value="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default FormContact
+
+const styles = {
+  FormContact: {
+    backgroundColor: 'var(--color-primary-light)',
+    // border: '2px solid var(--color-border)',
+  },
+}
--- sections/contact.js
+++ sections/contact.js
@@ -1,11 +1,21 @@
 import Section from '../components/section'
+import SectionContentBlock from '../components/sectionContentBlock'
+import FormContact from '../components/formContact'
 
 const Contact = () => (
   <Section id="contact">
-    <h3>Contact</h3>
-    <p>
-      Placeholder section.
-    </p>
+    <div className="row no-gutters">
+      <div className="col-lg-6 col-md-12 col-sm-12">
+        <SectionContentBlock heading="Contact Us">
+          <p>
+            Placeholder content.
+          </p>
+        </SectionContentBlock>
+      </div>
+      <div className="col-lg-6 col-md-12 col-sm-12">
+        <FormContact/>
+      </div>
+    </div>
   </Section>
 )
 
