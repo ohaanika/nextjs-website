446b4f7dca66b9155807e052026d389ede95ade2
Aanika Rahman <aanikarahman@gmail.com>
2021-12-09 12:26:06 -0500

    refactor(sections): update landing section

--- sections/landing.js
+++ sections/landing.js
@@ -1,14 +1,23 @@
 import Link from 'next/link'
 import Section from '../components/section'
+import CircleText from '../components/visuals/circleText'
 
 const Landing = () => (
-  <Section>
-    <h3>Professional Dash Apps by the Experts</h3>
-    <p>
-      With over 5 years of leading industry experience, we’re here to help you 
-      build production-ready, mobile-friendly, scalable Dash applications.
-    </p>
-    <button><Link href='#contact'><a>Contact</a></Link></button>
+  <Section id="team">
+    <div className="row no-gutters">
+      <div className="col-lg-6 col-md-12 col-sm-12" style={{paddingRight: "20px"}}>
+        <h3><CircleText color="var(--color-primary)">* Dash * Plotly *</CircleText></h3>
+      </div>
+      <div className="col-lg-6 col-md-12 col-sm-12">
+        <h2>Professional Dash Apps by the Experts</h2>
+        <h3 style={{fontWeight: "normal", textTransform: "none"}}>
+          With over <b>5</b> years of leading industry experience, we’re here to help you 
+          build production-ready, mobile-friendly, scalable Dash applications.
+        </h3>
+        <br/><br/>
+        <h2><Link href='#contact'><a>Contact us now</a></Link></h2>
+      </div>
+    </div>
   </Section>
 )
 
