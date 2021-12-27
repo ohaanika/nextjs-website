920e274df454c13f7023614cb34d40a45b0dd78a
Aanika Rahman <aanikarahman@gmail.com>
2021-12-27 09:41:55 -0500

    feat(contact): add calendly link

--- sections/contact.js
+++ sections/contact.js
@@ -1,23 +1,20 @@
 import Section from '../components/section'
 import SectionContentBlock from '../components/sectionContentBlock'
 import FormContact from '../components/formContact'
-import Callout from '../components/callout'
+import Callout from '../components/callouts/callout'
 
 const Contact = () => (
   <Section id="contact" backgroundColor="var(--color-primary-background)">
     <div className="row no-gutters">
       <div className="col-lg-6 col-md-12 col-sm-12 pr-lg-5">
-        <SectionContentBlock 
-          heading="Contact Us"
-          subheading="Want to work with us? Message us here or book a call to let us know what you have in mind."
-        >
+        <SectionContentBlock heading="Contact Us" subheading="Want to work with us? Message us here or book a call to let us know what you have in mind.">
           <div className="row mt-3">
-            <Callout label="Book a call on Calendly >" link="#contact"/>
+            <Callout url="https://calendly.com/zyphr-form/15min" text="Book a call on Calendly >" />
           </div>
         </SectionContentBlock>
       </div>
       <div className="col-lg-6 col-md-12 col-sm-12">
-        <FormContact/>
+        <FormContact />
       </div>
     </div>
   </Section>
