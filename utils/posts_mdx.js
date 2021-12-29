b93ce6e862636f2af2ad562f9f5c0e9d2e8ea905
Aanika Rahman <aanikarahman@gmail.com>
2021-12-29 04:32:46 -0500

    refactor(posts_mdx): use `fs.readdirSync`

--- utils/posts_mdx.js
+++ utils/posts_mdx.js
@@ -10,9 +10,9 @@ export const getSlugSource = (slug) => {
 }
 
 export const getAllPosts = () => {
-  const { readdirSync } = require('fs')
   const getDirectories = (path) =>
-    readdirSync(path, { withFileTypes: true })
+    fs
+      .readdirSync(path, { withFileTypes: true })
       .filter((dirent) => dirent.isDirectory())
       .map((dirent) => dirent.name)
 
