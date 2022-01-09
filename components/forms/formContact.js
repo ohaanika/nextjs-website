d50a3dfb9aeef888b364a221276a9fef6d8c912e
Aanika Rahman <aanikarahman@gmail.com>
2022-01-09 10:10:04 -0500

    feat(contact): send email after form submit

--- components/forms/formContact.js
+++ components/forms/formContact.js
@@ -1,38 +1,101 @@
+import { useState } from 'react'
 import styles from './formContact.module.css'
 
 const FormContact = () => {
   const label_required = <span style={{ color: 'var(--color-primary)' }}>{'﹡'}</span>
 
+  const [name, setName] = useState('')
+  const [affiliation, setAffiliation] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const [method, setMethod] = useState('default')
+  const [loading, setLoading] = useState(false)
+
+  const sendEmail = async (e) => {
+    e.preventDefault()
+
+    setLoading(true)
+    try {
+      const formData = new FormData()
+
+      if (!name.trim()) {
+        throw new Error('Please provide a valid name.')
+      }
+      if (!affiliation.trim()) {
+        throw new Error('Please provide a valid affiliation.')
+      }
+      if (!email.trim()) {
+        throw new Error('Please provide a valid email address.')
+      }
+      if (!message.trim()) {
+        throw new Error('Please provide a valid message.')
+      }
+      if (!method.trim()) {
+        throw new Error('Please provide a valid dropdown input for how you found about us.')
+      }
+
+      formData.append('name', name)
+      formData.append('affiliation', affiliation)
+      formData.append('email', email)
+      formData.append('message', message)
+      formData.append('method', method)
+
+      const response = await fetch('/api/contact', {
+        method: 'POST',
+        body: formData,
+      })
+
+      const responseData = await response.json()
+
+      if (responseData.error) {
+        console.log('Error:')
+        throw new Error(responseData.error)
+      }
+
+      console.log('Success!')
+
+      setName('')
+      setAffiliation('')
+      setEmail('')
+      setMessage('')
+      setMethod('')
+    } catch (error) {
+      console.log('Error!')
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <div className={styles.form}>
-      <form name="contact-form" method="POST" action="contact/?success=true">
+      <form name="contact-form" method="POST" action="contact/?success=true" onSubmit={sendEmail}>
         <fieldset>
           <label htmlFor="name">Name {label_required}</label>
-          <input id="name" name="name" type="text" placeholder="Your full name..." required />
+          <input id="name" name="name" type="text" placeholder="Your full name..." required value={name} onChange={(e) => setName(e.target.value)} />
         </fieldset>
         <fieldset>
           <label htmlFor="affiliation">Affiliation {label_required}</label>
-          <input id="affiliation" name="affiliation" type="text" placeholder="Your affiliation..." required />
+          <input id="affiliation" name="affiliation" type="text" placeholder="Your affiliation..." required value={affiliation} onChange={(e) => setAffiliation(e.target.value)} />
         </fieldset>
         <fieldset>
           <label htmlFor="email">Email {label_required}</label>
-          <input id="email" name="email" type="email" placeholder="Your email address..." required />
+          <input id="email" name="email" type="email" placeholder="Your email address..." required value={email} onChange={(e) => setEmail(e.target.value)} />
         </fieldset>
         <fieldset>
           <label htmlFor="message">Message {label_required}</label>
-          <textarea id="message" name="message" type="text" placeholder="Write your message here..." required />
+          <textarea id="message" name="message" type="text" placeholder="Write your message here..." required value={message} onChange={(e) => setMessage(e.target.value)} />
         </fieldset>
         <fieldset>
           <label htmlFor="method">How did you hear about us?</label>
-          <select name="method" id="method">
-            <option disabled selected value>
+          <select name="method" id="method" value={method} onChange={(e) => setMethod(e.target.value)}>
+            <option value="default" disabled>
               -- Select an Option --
             </option>
-            <option value="plotly">Plotly</option>
-            <option value="recommendation">Recommendation</option>
-            <option value="google">Google Search</option>
-            <option value="social-media">Social Media</option>
-            <option value="other">Other</option>
+            <option value="Plotly">Plotly</option>
+            <option value="Recommendation">Recommendation</option>
+            <option value="Google">Google Search</option>
+            <option value="Social Media">Social Media</option>
+            <option value="Other">Other</option>
           </select>
         </fieldset>
         <button type="submit" value="submit">
---
+++ pages/api/contact.js
@@ -0,0 +1,56 @@
+import nodemailer from 'nodemailer'
+import formidable from 'formidable'
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    type: 'OAuth2',
+    user: process.env.SENDER_EMAIL_ADDRESS,
+    pass: process.env.SENDER_EMAIL_PASSWORD,
+    clientId: process.env.OAUTH_CLIENT_ID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+  },
+})
+
+const parseFormData = (req) => {
+  var form = new formidable.IncomingForm()
+  return new Promise((resolve, reject) => {
+    form.parse(req, (error, fields, files) => {
+      if (error) reject(error)
+      else resolve({ fields, files })
+    })
+  })
+}
+
+const handler = async (req, res) => {
+  const { fields, files } = await parseFormData(req)
+
+  console.log(fields)
+
+  const mailOptions = {
+    from: process.env.SENDER_EMAIL_ADDRESS,
+    to: process.env.RECIEVER_EMAIL_ADDRESS,
+    replyTo: fields.email,
+    subject: `Inquiry: ${fields.affiliation} - ${fields.name}`,
+    text: `Email Address: ${fields.email} \n\nFound out about Zyphr by: ${fields.method} \n\n${fields.message}`,
+  }
+
+  console.log(mailOptions)
+
+  transporter.sendMail(mailOptions, function (error, response) {
+    if (error) {
+      console.log(`Cannot send email due to error: ${error}`)
+    } else {
+      console.log(`Email sent successfully: ${response}`)
+    }
+  })
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+export default handler
