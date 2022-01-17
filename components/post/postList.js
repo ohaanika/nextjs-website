1e9eb7531f99610f6ed6c80229923f244830c9b3
Aanika Rahman <aanikarahman@gmail.com>
2022-01-17 10:38:00 -0500

    refactor(post): update page spacing

--- components/post/postList.js
+++ components/post/postList.js
@@ -15,30 +15,30 @@ const PostListItem = ({ category, post, index }) => {
 }
 
 // METHOD 1
-const PostList = ({ category, posts }) => {
-  const Page = ({ backgroundColor = 'transparent', stretch = false, children }) => (
-    <div style={{ backgroundColor: backgroundColor, padding: 'calc(3 * var(--website-container-border)) 0' }}>
-      <div className={stretch === true ? '' : 'container'}>{children}</div>
-    </div>
-  )
-  return (
-    <Page>
-      <h1>{category}</h1>
-      {posts.map((post, index) => (
-        <PostListItem category={category} post={post} index={index} />
-      ))}
-    </Page>
-  )
-}
+// const PostList = ({ category, posts }) => {
+//   const Page = ({ backgroundColor = 'transparent', stretch = false, children }) => (
+//     <div style={{ backgroundColor: backgroundColor, padding: 'calc(3 * var(--website-container-border)) 0' }}>
+//       <div className={stretch === true ? '' : 'container'}>{children}</div>
+//     </div>
+//   )
+//   return (
+//     <Page>
+//       <h1>{category}</h1>
+//       {posts.map((post, index) => (
+//         <PostListItem category={category} post={post} index={index} />
+//       ))}
+//     </Page>
+//   )
+// }
 
 // METHOD 2 (draft)
-// const PostList = ({ category, posts }) => (
-//   <div className={styles.page}>
-//     <h1>{category}</h1>
-//     {posts.map((post, index) => (
-//       <PostListItem category={category} post={post} index={index} />
-//     ))}
-//   </div>
-// )
+const PostList = ({ category, posts }) => (
+  <div className={styles.page}>
+    <h1>{category}</h1>
+    {posts.map((post, index) => (
+      <PostListItem category={category} post={post} index={index} />
+    ))}
+  </div>
+)
 
 export default PostList
