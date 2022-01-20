e8afe23d685f8909204ffeae1536cfbe1db548a1
Aanika Rahman <aanikarahman@gmail.com>
2022-01-20 17:53:21 -0500

    refactor(posts): add images to index page

--- components/post/postList.js
+++ components/post/postList.js
@@ -1,43 +1,36 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import Frontmatter from './frontmatter'
 import styles from './post.module.css'
 
 const PostListItem = ({ category, post, index }) => {
+  let image = null
+  if ('image' in post.frontmatter) {
+    image = <Image src={`/images/portfolio/${post.slug}.png`} layout="fill" objectFit="cover" objectPosition="left top" />
+  } else {
+    image = <Image src={`/images/portfolio/admin-app.png`} layout="fill" objectFit="cover" objectPosition="left top" />
+  }
+
   return (
-    <div key={index} className={styles.item}>
-      <Link href={`/${category}/${post.slug}`}>
-        <a className={styles.link}>
+    <Link key={index} href={`/${category}/${post.slug}`}>
+      <a className={styles.link}>
+        <div className={styles.item}>
+          <div className={styles.image}>{image}</div>
           <Frontmatter frontmatter={post.frontmatter} />
-        </a>
-      </Link>
-    </div>
+        </div>
+      </a>
+    </Link>
   )
 }
 
-// METHOD 1
-// const PostList = ({ category, posts }) => {
-//   const Page = ({ backgroundColor = 'transparent', stretch = false, children }) => (
-//     <div style={{ backgroundColor: backgroundColor, padding: 'calc(3 * var(--website-container-border)) 0' }}>
-//       <div className={stretch === true ? '' : 'container'}>{children}</div>
-//     </div>
-//   )
-//   return (
-//     <Page>
-//       <h1>{category}</h1>
-//       {posts.map((post, index) => (
-//         <PostListItem category={category} post={post} index={index} />
-//       ))}
-//     </Page>
-//   )
-// }
-
-// METHOD 2 (draft)
 const PostList = ({ category, posts }) => (
   <div className={styles.page}>
     <h1>{category}</h1>
-    {posts.map((post, index) => (
-      <PostListItem category={category} post={post} index={index} />
-    ))}
+    <div className={styles.list}>
+      {posts.map((post, index) => (
+        <PostListItem category={category} post={post} index={index} />
+      ))}
+    </div>
   </div>
 )
 
