ec09917321bacc60b518a5d3517eb8ad2a391eaa
Aanika Rahman <aanikarahman@gmail.com>
2022-02-03 15:06:31 -0500

    refactor(portfolio): update gallery in home page

--- components/post/homePortfolioList.js
+++ components/post/homePortfolioList.js
@@ -4,26 +4,6 @@ import Image from 'next/image'
 import Frontmatter from './frontmatter'
 import styles from './post.module.css'
 
-const PostListItem = ({ category, post, index }) => {
-  let image = null
-  if ('image' in post.frontmatter) {
-    image = <Image src={`/images/${category}/${post.slug}.png`} layout="fill" objectFit="cover" objectPosition="left top" />
-  } else {
-    image = <Image src={`/images/portfolio/admin-app.png`} layout="fill" objectFit="cover" objectPosition="left top" />
-  }
-
-  return (
-    <Link key={index} href={`/${category}/${post.slug}`}>
-      <a className={styles.link}>
-        <div className={styles.item}>
-          <div className={styles.image}>{image}</div>
-          <Frontmatter frontmatter={post.frontmatter} />
-        </div>
-      </a>
-    </Link>
-  )
-}
-
 const HomePortfolioList = ({ category, posts }) => {
   const [selectedPostIndex, setSelectedPostIndex] = useState(0)
 
@@ -43,46 +23,24 @@ const HomePortfolioList = ({ category, posts }) => {
   }
 
   return (
-    <div
-      className="row no-gutters"
-      style={{ margin: '30px 0 0 0', width: '100%', height: '400px', padding: '30px', backgroundColor: 'var(--color-background-outer)', boxShadow: '0 0 20px rgba(0, 0, 0, 0.05)' }}
-    >
-      <div className="col-4" style={{ padding: '0 30px 0 0', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          {/* <Frontmatter frontmatter={posts[selectedPostIndex].frontmatter} /> */}
-          <h5>{posts[selectedPostIndex].frontmatter.title}</h5>
-          <p>{posts[selectedPostIndex].frontmatter.subtitle}</p>
-          <Link href={posts[selectedPostIndex].frontmatter.link}>Check out app</Link>
+    <div className={styles.slideshow}>
+      <div className={styles.slideshowDescription}>
+        <div className={styles.slideshowText}>
+          <Frontmatter frontmatter={posts[selectedPostIndex].frontmatter} />
           <Link href={`/${category}/${posts[selectedPostIndex].slug}`}>Read more</Link>
+          <Link href={posts[selectedPostIndex].frontmatter.link}>Check out app</Link>
         </div>
-        <div style={{ display: 'flex' }}>
-          <button style={{ backgroundColor: 'var(--color-primary-light)', marginRight: '30px' }} onClick={handleLeftClick}>
-            ←
-          </button>
-          <button style={{ backgroundColor: 'var(--color-primary-light)' }} onClick={handleRightClick}>
-            →
-          </button>
+        <div className={styles.slideshowArrows}>
+          <button onClick={handleLeftClick}>←</button>
+          <button onClick={handleRightClick}>→</button>
         </div>
       </div>
-      <div className="col-8" style={{ border: '2px solid var(--color-monotone-background)' }}>
-        {selectedPostImage}
+      <div className={styles.slideshowImage}>{selectedPostImage}</div>
+      <div className={styles.slideshowArrowsColumn}>
+        <button onClick={handleLeftClick}>←</button>
+        <button onClick={handleRightClick}>→</button>
       </div>
     </div>
-    // <div className="row no-gutters">
-    //   <div className="col-9">{posts[selectedPostIndex].frontmatter.title}</div>
-    //   <div className="col-3">
-    //     {posts.map((post, index) => (
-    //       <div key={index} data-index={index} style={{ margin: '20px 0', padding: '15px', backgroundColor: 'var(--color-monotone-background)' }} onClick={handleClick}>
-    //         {post.frontmatter.title}
-    //       </div>
-    //     ))}
-    //   </div>
-    // </div>
-    // <div className={styles.list}>
-    //   {posts.map((post, index) => (
-    //     <PostListItem category={category} post={post} index={index} />
-    //   ))}
-    // </div>
   )
 }
 
