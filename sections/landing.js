2e4134d914035f954f7620d26564034e31279f0f
Adam Kulidjian <adam.kulidjian@gmail.com>
2023-01-19 14:10:48 +0100

    feat(landing): add animating logo and reorganize text for better look and feel

--- sections/landing.js
+++ sections/landing.js
@@ -5,20 +5,20 @@ import Section from '../components/section/section'
 const Landing = () => (
   <Section backgroundColor="var(--color-primary-background)">
     <div className="row no-gutters">
-      <div className="col-lg-6 col-md-12 col-sm-12 pr-lg-5">
-        <Image src={'/images/logos/logo-icon.svg'} layout="fill" objectFit="contain" objectPosition="left" />
+      <div className="col-lg-5 col-md-12 col-sm-12">
+        <Image src={'/images/logos/logo-icon-circle-animation.svg'} alt="logo" layout="fill" objectFit="cover" objectPosition="right"/>
       </div>
 
-      <div className="col-lg-6 col-md-12 col-sm-12 pr-lg-5">
-        <h2>Dash Apps by the Experts</h2>
-        <h3 style={{ fontWeight: 300, textTransform: 'none', fontSize: '1.7rem' }}>
+      <div className="col-lg-7 col-md-12 col-sm-12">
+        <h2>Dash Apps <br/> by the Experts</h2>
+        <h3 style={{ fontWeight: 300, textTransform: 'none', fontSize: '1.4rem', lineHeight: '1.9rem', paddingTop: '2rem'}}>
           With over 5 years of leading industry experience, we’re here to help you build production-ready, mobile-friendly, scalable Dash applications.
         </h3>
         <br />
         <br />
         <h2>
           <Link href="#contact">
-            <a style={{fontSize: '1.7rem', textDecoration: 'none', backgroundColor: 'var(--color-primary)', color: 'white', padding: "0.3rem 0.6rem", borderRadius: "0.5rem"}}>Get in touch</a>
+            <a style={{fontSize: '1.4rem', textDecoration: 'none', backgroundColor: 'var(--color-primary)', color: 'white', padding: "0.2rem 0.5rem", borderRadius: "0.3rem"}}>Get in touch</a>
           </Link>
         </h2>
       </div>
