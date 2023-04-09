a509f3a6367577e3efa93087f0e4447020274390
Adam Kulidjian <adam.kulidjian@gmail.com>
2023-04-09 23:54:20 +0200

    feat(landing page): animate the background depending on where the mouse is

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import Team from '../sections/team'
 import Portfolio from '../sections/portfolio'
 import ClientTestimonials from '../sections/clientTestimonials'
 import Contact from '../sections/contact'
+import Script from 'next/script'
 
 import { getAllPostData } from '../utils/mdx'
 
@@ -31,6 +32,31 @@ const Index = ({ portfolioPosts }) => (
     <Portfolio posts={portfolioPosts} />
     <ClientTestimonials />
     <Contact />
+    <Script id="show-banner">
+{`
+let landingPage = document.getElementById("landing-page");
+
+landingPage.addEventListener("mousemove", (e) => {
+  let bounds = landingPage.getBoundingClientRect();
+  let x = event.clientX - bounds.left;
+  let y = event.clientY - bounds.top;
+
+  let subX = x / bounds["width"];
+  subX = subX.toFixed(2);
+
+  const leftHue = 77;
+  const rightHue = 176;
+  const delta = rightHue - leftHue;
+
+  let newLeftHue = leftHue + subX * delta;
+  let newRightHue = rightHue - subX * delta;
+
+  newLeftHue = parseInt(newLeftHue);
+  newRightHue = parseInt(newRightHue);
+  landingPage.style.backgroundImage = "linear-gradient(to right, hsl(" + newLeftHue + ", 22%, 87%), hsl(" + newRightHue + ", 40%, 87%))";
+})
+`}
+    </Script>
   </Layout>
 )
 
