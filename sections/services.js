20fd0a8b6101925c2d99635dd67b35ec1d8448f7
Adam Kulidjian <adam.kulidjian@gmail.com>
2023-04-16 00:58:54 +0200

    feat: fix spelling mistakes in the services copy

--- sections/services.js
+++ sections/services.js
@@ -5,26 +5,27 @@ import styles from './services.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 
+
 const ServiceCardInfo = [
   {
     title: "Dashboard Development",
     icon: "/images/icons/tablet-screen-button-solid.svg",
-    desc: "We code up your Dash apps from start to finish: MVPs, landing pages, scientific exploration tools are among our flavors. We even use Dash Design Kit for power users.",
+    desc: "We code up your Dash apps from start to finish: MVPs, dashboards, and scientific tools, are among our products. We even use Dash Design Kit for power users.",
   },
   {
     title: "Data-Driven Design",
     icon: "/images/icons/pencil-solid.svg",
-    desc: "Unlock meaningful insights from your data with the right designs. We help you design your charts, user flow, and all the custom CSS. ",
+    desc: "Unlock meaningful insights from your data with the right designs. We help you pick your visualizations, route intuitive user flows, and handle all detailed CSS.",
   },
   {
     title: "Code Review",
     icon: "/images/icons/code-branch-solid.svg",
-    desc: "Ensure you’re using best Dash practices. We bulletproof your callbacks for speed and performance, and meticulously code help test all the edge cases.",
+    desc: "Ensure you're using the best Dash practices. We bulletproof your callbacks for optimal performance, and adhere to PEP-8 Python standards.",
   },
   {
     title: "Pair Programming",
     icon: "/images/icons/lightbulb-solid.svg",
-    desc: "We also brainstorm features and work along side you in your coding environment. We’re a quick Slack huddle away.",
+    desc: "We can brainstorm features and work together in your tech stack of choice. We’re a quick Slack huddle away.",
   },
 ]
 
