9b5569119a6ebc8bea60099da7f8ad9eaf3a41c5
Adam Kulidjian <adam.kulidjian@gmail.com>
2023-04-16 01:43:39 +0200

    feat: make call button match landing page cta button style

--- components/callouts/calloutCalendly.js
+++ components/callouts/calloutCalendly.js
@@ -3,7 +3,7 @@ import { openPopupWidget } from 'react-calendly'
 const CalloutCalendly = ({ url, prefill, pageSettings, utm, text }) => {
   const onClick = () => openPopupWidget({ url, prefill, pageSettings, utm })
 
-  return <button onClick={onClick}>{text}</button>
+  return <button className="calendly-button" onClick={onClick}>{text}</button>
 }
 
 export default CalloutCalendly
