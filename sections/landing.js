c487e00dcda47c847228c282b15d8999ff51ecf9
Adam Kulidjian <adam.kulidjian@gmail.com>
2023-04-17 00:06:38 +0200

    perf: use smaller landing page icon to decrease render time

--- sections/landing.js
+++ sections/landing.js
@@ -6,13 +6,7 @@ const Landing = () => (
   <Section id="landing-page">
     <div className="row no-gutters">
       <div id="landing-page-logo-box" className="col-lg-5 col-md-12 col-sm-12">
-          <Image
-            className="landing-page-logo"
-            src={'/images/logos/logo-textured-with-backdrop.svg'}
-            alt="logo"
-            layout="fill"
-            objectPosition="right"
-          />
+        <img id="landingPageImg" src="/images/logos/newTexture.png"></img>
       </div>
 
       <div className="col-lg-7 col-md-12 col-sm-12">
